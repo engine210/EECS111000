8f0f956d49f6de6b4c775b9d5460c70ad48245cf
Steven Wang <engine210@gmail.com>
2019-01-08 14:13:21 +0800

    fix the bug in Bipartite Graph

--- Final_Practice/11763 - F - Bipartite Graph/main.c
+++ Final_Practice/11763 - F - Bipartite Graph/main.c
@@ -11,44 +11,22 @@
 
 int n;
 int edge[1010][1010];
-int depth;
+int flag;
 
-void show(void);
-
-void dfs(int vertice) {
+void dfs(int vertice, int depth) {
     int i;
-    //show();
     if (edge[0][vertice] == 0) {
         edge[0][vertice] = depth;
         for (i = 1; i <= n; i++) {
             if (edge[i][vertice]) {
-                depth++;
-                dfs(i);
-                depth--;
+                dfs(i, depth + 1);
             }
         }
     }
-}
-
-int judge() {
-    int i, j;
-    for (i = 1; i <= n; i++) {
-        for (j = i + 1; j <= n; j++) {
-            if (edge[i][j] && (edge[0][i] + edge[0][j]) % 2 == 0) {
-                return 0;
-            }
+    else {
+        if ((edge[0][vertice] + depth) % 2) {
+            flag = 0;
         }
-    }
-    return 1;
-}
-
-void show() {
-    int i, j;
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
-            printf("%d ", edge[i][j]);
-        }
-        printf("\n");
     }
 }
 
@@ -64,15 +42,16 @@ int main(int argc, const char * argv[]) {
     while (T > 0) {
         scanf("%d%d", &n, &m);
         memset(edge, 0, sizeof(edge));
-        for (i = 1; i <= m; i++) {
+        for (i = 0; i < m; i++) {
             scanf("%d%d", &vertice_1, &vertice_2);
             edge[vertice_1][vertice_2] = 1;
             edge[vertice_2][vertice_1] = 1;
         }
         
-        depth = 1;
-        dfs(1);
-        judge() ? printf("Yes\n") : printf("No\n");
+        flag = 1;
+        dfs(1, 1);
+        flag ? printf("Yes\n") : printf("No\n");
+        
         
         T--;
     }
